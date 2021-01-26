c6bc32930572852f20dc5e6784b24049ee0e1e2f
Praabindh Pradeep <63050949+praabindhp@users.noreply.github.com>
2021-01-26 23:09:35 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,17 @@
+# Welcome Note
+print("Howdy, I'm Prabindh")
+# Defining The Function
+def factorial(n):
+	if n < 0: 
+		return 0
+	elif n == 0 or n == 1: 
+		return 1
+	else: 
+		fact = 1
+		while(n > 1):
+			fact *= n 
+			n -= 1
+		return fact
+
+n=int(input("Enter The Number To Find The Factorial"))
+print("The Factorial Of",n,"is",factorial(n))
